81cf636cd28a4c45edaa7f85c321bdde1eb7942f
ANANTH4004 <balamuruganananth@gmail.com>
2022-08-18 21:42:02 +0530

    first commit

---
+++ day3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = @"D:\c#\day3\hello.txt";
+            if (File.Exists(file))
+            {
+                string str = File.ReadAllText(file);
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
